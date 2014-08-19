ae594078e50520b791d85b0c542a875495c41806
prophet777 <johann_27@hotmail.fr>
2014-08-20 00:03:25 +0200

    Add user processor for monolog

---
+++ src/Certificationy/Bundle/WebBundle/Log/Processor/UserProcessor.php
@@ -0,0 +1,54 @@
+<?php
+/**
+* This file is part of the PhpStorm.
+* (c) johann (johann_27@hotmail.fr)
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+**/
+
+namespace Certificationy\Bundle\WebBundle\Log\Processor;
+
+use Certificationy\Bundle\UserBundle\Entity\User;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationCredentialsNotFoundException;
+use Symfony\Component\Security\Core\SecurityContext;
+
+class UserProcessor
+{
+    /**
+     * @var \Symfony\Component\DependencyInjection\ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param $record
+     */
+    public function postRecord($record)
+    {
+        $securityContext = $this->container->get('security.context');
+
+        try{
+            if($securityContext->isGranted('IS_AUTHENTICATED_FULLY')){
+
+                /** @var User $user */
+                $user = $securityContext->getToken()->getUser();
+
+                $record['extra']['user']['name'] = $user->getRealName();
+                $record['extra']['user']['email'] = $user->getEmailCanonical();
+                $record['extra']['user']['id'] = $user->getId();
+                return $record;
+            }
+        } catch (AuthenticationCredentialsNotFoundException $e){}
+
+        return $record;
+    }
+} 
