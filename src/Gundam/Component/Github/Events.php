7664e6162d52d673920e14cf068b2bdf8ba15571
prophet777 <johann_27@hotmail.fr>
2014-08-21 02:24:51 +0200

    Add github event on gundam

--- src/Gundam/Component/Github/Events.php
+++ src/Gundam/Component/Github/Events.php
@@ -14,4 +14,21 @@ final class Events
     const PULL_REQUEST = 'pull_request';
     const PING = 'ping';
     const PUSH = 'push';
+    const DELETE = 'delete';
+    const DEPLOYMENT = 'deployment';
+    const COMMIT_COMMENT = 'commit_comment';
+    const ISSUE_COMMENT = 'issue_comment';
+    const FORK = 'fork';
+    const MEMBER = 'member';
+    const RELEASE = 'release';
+    const PAGE_BUILD = 'page_build';
+    const CREATE = 'create';
+    const STATUS = 'status';
+    const DEPLOYMENT_STATUS = 'deployment_status';
+    const PULL_REQUEST_REVIEW_COMMENT = 'pull_request_review_comment';
+    const ISSUES = 'issues';
+    const WATCH = 'watch';
+    const VISIBILITY = 'public';
+    const TEAM_ADD = 'team_add';
+    const GOLLUM = 'gollum';
 }
